c08b3f93a34abb1213916ac310adfc9845b97309
akira-sasaki <gclue.akira@gmail.com>
2019-08-20 11:55:45 +0900

    Support python 3.x

--- example/read9axis.py
+++ example/read9axis.py
@@ -19,20 +19,20 @@ mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 try:
     while True:
         accel = mpu9250.readAccel()
-        print " ax = " , ( accel['x'] )
-        print " ay = " , ( accel['y'] )
-        print " az = " , ( accel['z'] )
+        print(" ax = " , ( accel['x'] ))
+        print(" ay = " , ( accel['y'] ))
+        print(" az = " , ( accel['z'] ))
 
         gyro = mpu9250.readGyro()
-        print " gx = " , ( gyro['x'] )
-        print " gy = " , ( gyro['y'] )
-        print " gz = " , ( gyro['z'] )
+        print(" gx = " , ( gyro['x'] ))
+        print(" gy = " , ( gyro['y'] ))
+        print(" gz = " , ( gyro['z'] ))
 
         mag = mpu9250.readMagnet()
-        print " mx = " , ( mag['x'] )
-        print " my = " , ( mag['y'] )
-        print " mz = " , ( mag['z'] )
-        print
+        print(" mx = " , ( mag['x'] ))
+        print(" my = " , ( mag['y'] ))
+        print(" mz = " , ( mag['z'] ))
+        print()
 
         time.sleep(0.5)
 
